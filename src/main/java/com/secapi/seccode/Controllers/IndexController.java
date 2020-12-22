92cdf729a82b02213c575d1300c30258f5adc0b2
3therk1ll <richardcurteis@gmail.com>
2020-12-22 14:30:12 -0800

    sanitisation tester working

--- src/main/java/com/secapi/seccode/Controllers/IndexController.java
+++ src/main/java/com/secapi/seccode/Controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.secapi.seccode.Controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -14,12 +15,26 @@ public class IndexController {
 	}
 
 	@GetMapping("/name")
-	public String getName(@RequestParam( name= "name", required = false, defaultValue = "World") String name ) {
+	public String getName(@RequestParam( name = "name", required = false, defaultValue = "World") String name ) {
 		return "{  \"welcomeMessage\" : \"Welcome, " + upcaseName(name) + ", to a most secure API\" }";
+	}
+
+	@GetMapping("/sanitise")
+	public String handleInput(@RequestParam( name = "input", required = false) String input ) {
+		if(input == null || input == "") {
+			return "{  \"output\" : \"No argument found\" }";
+		}
+		else {
+			return "{  \"output\" : " + sanitiseInput(input) + " }";
+		}
 	}
 
 	private static String upcaseName(String name) {
 		return name.substring(0, 1).toUpperCase() + name.substring(1);
+	}
+
+	private static String sanitiseInput(String input) {
+		return input.replace("'", "").replace(";", "").replace(" ", "");
 	}
 
 }
